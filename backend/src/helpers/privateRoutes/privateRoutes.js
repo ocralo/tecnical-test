24c0c2a2a716e15e1b35197f65711b9152657f5b
ocralo <ocralo@gmail.com>
2021-03-23 22:17:32 -0500

    create privateroutes

---
+++ backend/src/helpers/privateRoutes/privateRoutes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const protectedRoutes = express.Router();
+const jwt = require("jsonwebtoken");
+
+const configJwt = require("./../../config/jwtKey/keyJWTConfig");
+
+protectedRoutes.use((req, res, next) => {
+	const inToken =
+		req.headers["auth"] ||
+		req.headers["authorization"] ||
+		req.headers["Authorization"];
+
+	console.log("token", inToken);
+
+	const token = inToken ? inToken.replace("Bearer ", "") : "";
+
+	if (token) {
+		jwt.verify(token, configJwt.key, (err, decoded) => {
+			if (err) {
+				return res.json({ message: "Token inválida", error: true });
+			} else {
+				req.decoded = decoded;
+				console.log("decode", decoded);
+				next();
+			}
+		});
+	} else {
+		res.send({
+			message: "Token no enviado.",
+			error: true,
+		});
+	}
+});
+
+module.exports = protectedRoutes;
